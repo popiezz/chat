00884a64f06ad568464f4cf153fcca2eb555ceb4
popiezz <philpopescu@outlook.com>
2025-07-18 09:40:33 -0400

    user formatting

--- main.go
+++ main.go
@@ -31,8 +31,26 @@ func showWelcome(msg []byte, conn net.Conn) {
 	log.Printf("Accepted connection from %s\n", SafeRemoteAddress(conn))
 }
 
+func getUsername(conn net.Conn) string {
+	tutorial := []byte("Type your username : ")
+	_, err := conn.Write(tutorial)
+	if err != nil {
+		log.Printf("ERROR getting the username: %s", err)
+	}
+	reader := bufio.NewReader(conn)
+	user, err := reader.ReadString('\n')
+	if err != nil {
+		log.Printf("ERROR getting username : %s", err)
+	}
+	msgStart := []byte("You can now start typing! Enjoy!\n")
+	conn.Write(msgStart)
+	userLen := len(user)
+	username := user[:userLen-1]
+	return username
+}
+
 // function needed to read the message from the client
-func acceptMessage(conn net.Conn) {
+func acceptMessage(conn net.Conn, user string) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
 	for {
@@ -47,21 +65,24 @@ func acceptMessage(conn net.Conn) {
 		}
 
 		cleanMsg := strings.TrimSpace(msg)
+		cleanUser := strings.TrimSpace(user)
 		upperMsg := strings.ToUpper(cleanMsg)
 
 		if strings.Contains(upperMsg, "BYE") {
 			conn.Write([]byte("Goodbye!\n"))
-			log.Println("Client has closed the connection.")
+			log.Printf("%s has closed their connection.", user)
 			os.Exit(0)
 		}
-		log.Printf("Client : %s", msg)
+		log.Printf("%s : %s", cleanUser, cleanMsg)
 	}
 }
 
 func HandleConnection(conn net.Conn) {
 	message := []byte("\n       --- Welcome to PipChat --- \n If you'd like to exit, please type 'BYE'\n")
 	showWelcome(message, conn)
-	go acceptMessage(conn)
+	user := getUsername(conn)
+	log.Printf("Address %s has chosen username %s", conn.RemoteAddr().String(), user)
+	go acceptMessage(conn, user)
 
 	// function needed to read the message from the server
 	go func() {
